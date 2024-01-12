d0d0740f69dd53f31de608de32f6e67088d8ef9c
Soulnature <naturescarl@gmail.com>
2024-01-12 14:10:09 +0800

    code_ad

--- SACN/main.py
+++ SACN/main.py
@@ -69,7 +69,7 @@ def main(root_dir, network_type, con_st,DOMAIN_L,model_name,base,epoches,batch_s
     data = pd.read_csv(root_dir)
    # data=data[0:3000]
     train_d, test_set = split_dataset(data)
-    test_d, test_e = train_test_split(test_set, test_size=0.33, random_state=0)
+    test_d, test_e = train_test_split(test_set, test_size=0.66, random_state=0)
 
     train_dataset = image_domian_gender(train_d['Loc'].values, train_d['Gender'].values, train_d['Scanner'].values, train_d['Age'].values)
     val_dataset = image_domian_gender(test_d['Loc'].values, test_d['Gender'].values, test_d['Scanner'].values, test_d['Age'].values)
